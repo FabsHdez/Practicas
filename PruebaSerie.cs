be7392a7e1b2da548a870c2d94b5d7ce87ebceea
FabsHdez <56403515+FabsHdez@users.noreply.github.com>
2020-03-08 18:46:01 -0700

    Add files via upload

---
+++ PruebaSerie.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace PruebaSerie
+{
+    class PruebaSerie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binvenido a la clase Serie, \n" + "Por favor ingrese los datos siguientes para completar la informacion de la serie: \n");
+            int temporadas;
+            string titulo;
+            string genero;
+            string creador;
+
+            int opcionConstructor = 0;
+            
+
+            Console.WriteLine(" Podrias insertar el titulo: \n");
+            titulo = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine(" Podrias insertar el genero: \n");
+            genero = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine(" Podrias insertar las temporadas: \n");
+            temporadas = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+            Console.WriteLine(" Podrias insertar el creador: \n");
+            creador = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Este apartado es para escoger un constructor que muestre los datos que pusiste, \n" + "Por favor, escoge alguna de nuestros 3 constructores: \n" +
+        "1)   Constructor #1 \n" +
+              "Descrpcion: mostrara todo los datos insertados anteriormente excepto la entrega \n" +
+
+        "2)   Constructor #2 \n" +
+              "Descrpcion: mostrara los datos titulo y creador que insertastes, todo lo demas datos se generaran por defecto \n");
+            Console.WriteLine("\n" +
+
+          "3)   Constructor #3 \n" +
+                "Descripcion: mostrara todo los datos por defecto excepto entregar \n");
+
+
+            do
+            {
+
+                Console.WriteLine("Opcion a escoger: \n");
+
+                opcionConstructor = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Opcion escogida \n" + opcionConstructor + "\n");
+                Console.WriteLine("Error, por favor introduzca un numero que coincide con las opciones mostradas: \n" +
+
+                 "1) Constructor #1 \n" +
+              "Descrpcion: mostrara todo los datos insertados anteriormente excepto la entrega \n" +
+
+          "2) Constructor #2 \n" +
+              "Descrpcion: mostrara los datos titulo y creador que insertastes, todo lo demas datos se generaran por defecto \n");
+                Console.WriteLine("\n" +
+
+          "3) Constructor #3 \n" +
+                    "Descripcion: mostrara todo los datos por defecto excepto entregar \n");
+            } while (opcionConstructor != 1 && opcionConstructor != 2 && opcionConstructor != 3);
+         
+            switch (opcionConstructor)
+                    {
+                        case 1:
+                            Console.WriteLine("Opcion escogida: \n" + opcionConstructor + ") Constructor #1: \n");
+                            Serie Serie1 = new Serie();
+                            Serie1.setTitulo(titulo);
+                            Serie1.setGenero(genero);
+                            Serie1.setTemporadas(temporadas);
+                            Serie1.setCreador(creador);
+
+                            Console.WriteLine(Serie1.MuestraMensaje());
+
+                            break;
+                        case 2:
+                            Console.WriteLine("Opcion escogida: \n" + opcionConstructor + ") Constructor #2: \n");
+                            Serie Serie2 = new Serie(titulo, creador);
+                            Serie2.setTitulo("");
+                            Serie2.setCreador("");
+                            Console.WriteLine(Serie2.MuestraMensaje1());
+
+                            break;
+                        case 3:
+                            Console.WriteLine("Opcion escogida: \n" + opcionConstructor + ") Constructor #3: \n");
+                            Serie Serie3 = new Serie();
+                            Console.WriteLine("Serie 3");
+                            Console.WriteLine(Serie3.MuestraMensaje2());
+                            break;
+                    }
+                }
+
+
+        
+
+    }
+}
+
+
+            
+
+           
+        
+
+
+
+   
---
+++ Serie.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaSerie
+{
+    class Serie
+    {
+        private string titulo;
+        public void getTitulo(String titulo)
+        {
+            this.titulo = titulo;
+        }
+        public void setTitulo(string titulo)
+        {
+            this.titulo = titulo;
+        }
+        private int temporadas;
+        public void getTemporadas(int temporadas)
+        {
+            this.temporadas = temporadas;
+        }
+        public void setTemporadas(int temporadas)
+        {
+            this.temporadas = temporadas;
+        }
+
+
+        private bool entregado;
+
+
+        private string genero;
+        public void getGenero(string genero)
+        {
+            this.genero = genero;
+        }
+        public void setGenero(string genero)
+        {
+            this.genero = genero;
+        }
+
+
+        private string creador;
+        public void getCreador(string creador)
+        {
+            this.creador = creador;
+        }
+        public void setCreador(string creador)
+        {
+            this.creador = creador;
+        }
+
+
+        public Serie()
+        {
+            titulo = "";
+            temporadas = 3;
+            entregado = false;
+            genero = "";
+            creador = "";
+        }
+
+        public Serie(String titulo, string creador)
+        {
+            this.titulo = titulo;
+            this.creador = creador;
+
+        }
+        public Serie(String titulo, int temporadas, string genero, string creador)
+        {
+            this.titulo = titulo;
+            this.temporadas = temporadas;
+            this.genero = genero;
+            this.creador = creador;
+        }
+
+       
+        public String MuestraMensaje()
+        {
+
+            return "Informacion de la persona: \n"
+                    + "Titulo: " + titulo + "\n"
+                    + "Temporadas: " + temporadas + "\n"
+                    + "Entregado: " + entregado + " \n"
+                    + "Genero: " + genero + "\n"
+                    + "Creador: " + creador + " \n" +
+
+
+            "Gracias por participar! \n" + "Por favor precione cualquier tecla para cerrar el programa";
+
+
+        }
+        public String MuestraMensaje1()
+        {
+
+            return "Informacion de la persona: \n"
+                    + "Titulo: " + titulo + "\n"
+                    + "Temporadas: 3 \n"
+                    + "Entregado: " + entregado + " \n"
+                    + "Genero: accion \n"
+                    + "Creador: " + creador + " \n" +
+            "Gracias por participar! \n" + "Por favor precione cualquier tecla para cerrar el programa";
+
+        }
+        public String MuestraMensaje2()
+        {
+
+            return "Informacion de la persona: \n"
+                    + "Titulo: Mendez \n"
+                    + "Temporadas: 18 \n"
+                    + "Entregado:" + entregado + "\n"
+                    + "Genero: Accion \n"
+                    + "Creador: Paulini Cota \n" +
+
+            "Gracias por participar! \n" + "Por favor precione cualquier tecla para cerrar el programa";
+        }
+    }
+}
